c43c5ff5e1c60a705c5387f5e589fc4be3cb35c0
Zachary Lee <zacharylhlee@gmail.com>
2018-10-02 16:31:54 +0000

    Adds a GET route to show urls_new.ejs

--- express_server.js
+++ express_server.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var app = express();
 var PORT = 8080;
+const bodyParser = require("body-parser");
+
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -17,6 +20,10 @@ app.get("/urls", (req, res) => {
   let templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+})
 
 app.get("/urls/:id", (req, res) => {
   let templateVars = {shortURL: req.params.id, urls: urlDatabase};
